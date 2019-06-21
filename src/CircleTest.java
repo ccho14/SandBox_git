34c732879c3a9538307a384d980415e56aef1f3c
SeanCho <chunghwan14@gmail.com>
2019-06-20 23:24:06 -0400

    a + b

--- src/CircleTest.java
+++ src/CircleTest.java
@@ -5,8 +5,14 @@ public class CircleTest {
         System.out.println("Hello");
         System.out.println("Currently just testing Github");
 // Testing
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        a += 5;
+        b += 6;
+        c = a + b;
 
-        a + b = 10;
+        System.out.println(c);
     }
 
 }
