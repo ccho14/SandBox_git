294d309281c121b26c1143730f57e9fb8cd6ae39
SeanCho <chunghwan14@gmail.com>
2019-06-20 22:55:40 -0400

    initial commit

--- src/CircleTest.java
+++ src/CircleTest.java
@@ -3,7 +3,7 @@ public class CircleTest {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-
+        System.out.println("Currently just testing Github");
     }
 
 }
