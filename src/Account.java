de06fc077a90d745fb9949a1ca806f0a3d321fd1
SeanCho <chunghwan14@gmail.com>
2019-07-08 22:38:08 -0400

    BankAccount

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,65 @@
 public class Account {
+
+    private String number;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+    public double balance;
+
+    public void deposit(double depositAmount) {
+        this.balance += depositAmount;
+        System.out.println("");
+
+    }
+
+    public void withdrawal(double withdrawalAmount) {
+        if (this.balance - withdrawalAmount <= 0) {
+            System.out.println("Only" + this.balance + " available. Withdrawal not processed");
+        } else {
+
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount);
+        }
+
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main(String[] args) {
 
         Account seanAccount = new Account();
-        seanAccount.withdrawl(100.0);
+        seanAccount.withdrawal(100.0);
 
         seanAccount.deposit(50.0);
-        seanAccount.withdrawl(100.0);
+        seanAccount.withdrawal(100.0);
 
         seanAccount.deposit(51.0);
-        seanAccount.withdrawl(100.0);
+        seanAccount.withdrawal(100.0);
 
     }
 
